f41a226418c86ef94e9c1f6224c2ffcde1c1c251
Sergey Zaligin <webdevjsphp@gmail.com>
2018-11-10 14:10:11 +0300

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+
+const config = require('./config/keys');
+
+mongoose
+  .connect(config.mongoUrl)
+  .then(() => {
+    console.log('Connection mongodb success!!!');
+  })
+  .catch(error => console.log(error));
 
 const app = express();
 
